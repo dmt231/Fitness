4f65c1373f3ec42fde96c83fdb64626384b80a18
dmt231 <dominhtue123@gmail.com>
2024-04-13 00:11:25 +0700

    Update Login and Sign Up Layout

--- app/src/main/java/com/example/fitness/authentication/LoginFragment.kt
+++ app/src/main/java/com/example/fitness/authentication/LoginFragment.kt
@@ -18,9 +18,27 @@ class LoginFragment : Fragment() {
         viewBinding.btnBack.setOnClickListener {
             onBack()
         }
+        setUpAnimation()
         return viewBinding.root
     }
     private fun onBack(){
         requireActivity().supportFragmentManager.popBackStack()
+    }
+    private fun setUpAnimation(){
+        viewBinding.cardViewLogin.alpha = 0F
+        viewBinding.cardViewLogin.animate().alpha(1F).setDuration(600).start()
+
+        viewBinding.emailLogin.alpha = 0F
+        viewBinding.emailLogin.translationX = -200F
+        viewBinding.emailLogin.animate().translationX(0F).alpha(1F).setDuration(800).start()
+
+        viewBinding.passwordLogin.alpha = 0F
+        viewBinding.passwordLogin.translationX = -200F
+        viewBinding.passwordLogin.animate().translationX(0F).alpha(1F).setDuration(1000).start()
+
+        viewBinding.buttonLogin.alpha = 0F
+        viewBinding.buttonLogin.translationX = -200F
+        viewBinding.buttonLogin.animate().translationX(0F).alpha(1F).setDuration(1200).start()
+
     }
 }
--- app/src/main/java/com/example/fitness/authentication/SignUpFragment.kt
+++ app/src/main/java/com/example/fitness/authentication/SignUpFragment.kt
@@ -18,9 +18,34 @@ class SignUpFragment : Fragment() {
         viewBinding.btnBack.setOnClickListener {
             onBack()
         }
+        setUpAnimation()
         return viewBinding.root
     }
     private fun onBack(){
         requireActivity().supportFragmentManager.popBackStack()
+    }
+    private fun setUpAnimation(){
+        viewBinding.cardViewSignUp.alpha = 0F
+        viewBinding.cardViewSignUp.animate().alpha(1F).setDuration(500).start()
+
+        viewBinding.firstName.alpha = 0F
+        viewBinding.firstName.translationX = -200F
+        viewBinding.firstName.animate().translationX(0F).alpha(1F).setDuration(800).start()
+
+        viewBinding.lastName.alpha = 0F
+        viewBinding.lastName.translationX = -200F
+        viewBinding.lastName.animate().translationX(0F).alpha(1F).setDuration(900).start()
+
+        viewBinding.emailSignUp.alpha = 0F
+        viewBinding.emailSignUp.translationX = -200F
+        viewBinding.emailSignUp.animate().translationX(0F).alpha(1F).setDuration(1000).start()
+
+        viewBinding.passwordSignUp.alpha = 0F
+        viewBinding.passwordSignUp.translationX = -200F
+        viewBinding.passwordSignUp.animate().translationX(0F).alpha(1F).setDuration(1100).start()
+
+        viewBinding.buttonSignUp.alpha = 0F
+        viewBinding.buttonSignUp.translationX = -200F
+        viewBinding.buttonSignUp.animate().translationX(0F).alpha(1F).setDuration(1200).start()
     }
 }
